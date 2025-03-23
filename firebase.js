baf98bee658dcc0b35c57be797a6ece8a91bd59e
AJNAGAA <101543053+AJNAGAA@users.noreply.github.com>
2025-03-23 14:20:13 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,5 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
+// firebase-config.js
+// Replace the below values with your Firebase project credentials
 const firebaseConfig = {
   apiKey: "AIzaSyDpOnti9yv1DQa5mspgG02B_mPP4SruopM",
   authDomain: "moviehub-3c7db.firebaseapp.com",
@@ -11,7 +9,6 @@ const firebaseConfig = {
   appId: "1:137910493083:web:a92dc3bc47ceedd3d1cb3e"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
